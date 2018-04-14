36b5b86bf8b0f5d6504eb2a45cac4f62e9c8fb41
GuilhermeCarvalho1144 <37717912+GuilhermeCarvalho1144@users.noreply.github.com>
2018-04-14 14:29:39 -0300

    Update temp_data_loading.py

--- temp_data_loading.py
+++ temp_data_loading.py
@@ -12,7 +12,7 @@ import pandas as pd
 file_name = 'international-airline-passengers.csv'       ##ENTER THE FILE NAME
 #LOADING THE FILE...CONFIGURE THE READ FUNCTION SUIT YOUR DATA
 df = pd.read_csv(file_name, engine = 'python', skipfooter=3)
-#RENAMING THE COLUMMS
+#RENAMING THE COLUMNS
 df.rename(index = str, columns={"International airline passengers: monthly totals in thousands. Jan 49 ? Dec 60":"Passengers"}, inplace = True)
 #PREVIEWING
 print df.head(5)
